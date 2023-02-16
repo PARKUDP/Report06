9a38028f29564041c477dcc14eb0da790c3a010a
Keigo Shinjo <e225755@ie.u-ryukyu.ac.jp>
2023-02-17 04:09:50 +0900

    PARK CHEOLHWANのコード

--- app/src/test/java/jp/ac/uryukyu/ie/e225752_e225755_e225758/CardlistTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e225752_e225755_e225758/CardlistTest.java
@@ -1,12 +1,14 @@
 package jp.ac.uryukyu.ie.e225752_e225755_e225758;
 import java.util.Arrays;
+import jp.ac.uryukyu.ie.e225752_e225755_e225758.*;
 
 import org.junit.jupiter.api.Test;
 
 class CardlistTest {
     @Test
     void cardlistTest(){
-        Cardlist cardlist = new Cardlist();
-        System.out.println(cardlist.card_list);
+        Cardlist clist = new Cardlist();
+        clist.Card_remove("Black Jocker");
+        clist.Card_remove("Color Joker");
     }
 }
