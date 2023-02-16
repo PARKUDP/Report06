ea7e5b42f69ab71f45674250db20702689d2f66a
Keigo Shinjo <e225755@ie.u-ryukyu.ac.jp>
2023-02-16 18:56:17 +0900

    Human.javaを修正

--- app/src/main/Main.java
+++ app/src/main/Main.java
@@ -9,7 +9,11 @@ public class Main {
         //プレイヤーが二人の場合
         Cardlist cl = new Cardlist();
         Player plyer1 = new Player(player1);
-        Player plyer2 = new Player(player2);
+        Deeler deeler1 = new Deeler();
+
+        String drawedcard = Player1.draw();
+        System.out.println("プレイヤー１は" + drawedcard + "を引いた。" + "得点は" + Player1.totalValue());
+
 
 
     }
--- app/src/main/java/jp/ac/uryukyu/ie/e225752_e225755_e225758/Human.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225752_e225755_e225758/Human.java
@@ -1,7 +1,10 @@
 package jp.ac.uryukyu.ie.e225752_e225755_e225758;
 import java.util.Random;
-import java.util.Arraylist;
+import java.lang.constant.DirectMethodHandleDesc;
+import java.util.ArrayList; //スペルミスを修正
 import org.apache.commons.lang3.StringUtils;
+import jp.ac.uryukyu.ie.e225752_e225755_e225758.Cardlist;
+
 
 
 public class Human {
@@ -9,58 +12,88 @@ public class Human {
 
     //フィールド記述。
     String name;
-    Arraylist hand = new Arraylist<>();
+    ArrayList<String> hand = new ArrayList<>();//スペルミスを修正
     int score = 0;
+    Cardlist clist; //追加（山札用）
     
+    public Human(String _name, Cardlist _cardlist){ //コンストラクタ追加
+        this.name = _name;
+        this.clist = _cardlist;
+    }
 
     
 
     //メソッドを記述。
     //ランダムを用いてカードリストから選び、手札に追加するメソッド。
     public String draw(){
-        Random rd = new Random;
-        int randint = rd.nextInt(card_list.size());
-        ArrayList card_list = Cardlist.getCardList();
-        String drawed_card = card_list[randint];
+        Random rd = new Random(); //()を追加
+        int randint = rd.nextInt(clist.card_list.size());
+        //System.out.println(randint);
+        //Cardlist card_list = clist.card_list.getCardList(); //クラスメソッドを直接使うのではなく、オブジェクトのメソッドを使う
+        String drawed_card = clist.card_list.get(randint); //配列インデックス指定ではなく、getメソッドを使う
         hand.add(drawed_card);
-        Cardlist.remove(drawed_card);
+        clist.card_list.remove(drawed_card); //オブジェクトのメソッドを使う
+        System.out.println("draw(): " + hand);
 
         return drawed_card;
     }
 
     
+    
     //ドローした後の合計得点を示すメソッド。
     public int totalValue(){
         int totalvalue = 0;
+        int joker =0;
+
         for(int i = 0; i < hand.size(); i++){
-            String card = hand[i];
-            String cardvalue = card.charAt(2);
-            
-            if(boolean isNumeric = StringUtils.isNumeric(cardvalue)){
-                totalvalue += cardvalue;
-            }
-            if(cardvalue == "J" || cardvalue == "Q" || cardvalue =="K"){
+            String card = hand.get(i);
+            char last = card.charAt(card.length() - 1);
+            String cardvalue = String.valueOf(last);
+
+            //取り出した値が数字の場合
+            if(Character.isDigit(last)){
+                totalvalue += Integer.parseInt(cardvalue);
+                if(Integer.parseInt(cardvalue) == 0){
+                    totalvalue += 10;
+                }                           
+            }           
+            //取り出した値がJ,Q,Kの場合
+            if(cardvalue.equals("J") || cardvalue.equals("Q") || cardvalue.equals("K")){
                 totalvalue += 10;
             }
-            if(cardvalue == "A"){
-                if(totalvalue <= 21){
+            //取り出した値がAの場合
+            if(cardvalue.equals("A")){
+                if(totalvalue <= 10){
                     totalvalue += 11;
                 }else{
                     totalvalue += 1;
                 }
             }
+            //取り出した値がJokerの場合
+            if(cardvalue.equals("r")){
+                joker += 1;
+            }
         }
+        for(int i = 0; i < joker; i++){
+            if(totalvalue <= 10){
+                totalvalue += 11;
+            }else if(totalvalue == 11){
+                totalvalue += 10;
+            }else{
+                totalvalue += 21 - totalvalue;
+            }  
+        }       
+        System.out.println(totalvalue);
         return totalvalue;
     }
+
+    //試運転
     
+    public static void main(String[] args) {
+        Cardlist clist = new Cardlist();
+        Human demo = new Human("demo", clist);
+        demo.draw();
+        demo.draw();
+        demo.totalValue();  
+    }    
 }
-
-
-
-
-/*
- * カードリストのメソッドを、カードをの出し入れを統一する。
- * 新しく、Humanクラスを使い、今までのPlayerクラスからdealerクラスへ継承からの変更。
- * 点数計算方法を考案する。
- * ルールのクラスを作成？？
- */
