0c15d921252ab1d2def7cf2bc3eb8975b72b9f2c
ogiek0628 <117128611+ogiek0628@users.noreply.github.com>
2023-02-17 04:11:46 +0900

    PARK CHEOLHWAN

--- app/src/main/Main.java
+++ app/src/main/Main.java
@@ -1,13 +1,40 @@
+package jp.ac.uryukyu.ie.e225752_e225755_e225758;
+
 public class Main {
     public static void main(String[] args){
         //プレイヤーが二人の場合
         Cardlist cl = new Cardlist();
-        Player plyer1 = new Player(player1);
-        Deeler deeler1 = new Deeler();
-
-        String drawedcard = Player1.draw();
-        System.out.println("プレイヤー１は" + drawedcard + "を引いた。" + "得点は" + Player1.totalValue());
-
+        cl.Card_remove("Color Joker");
+        cl.Card_remove("Black Joker");
+        Player plyer1 = new Player("Aさん", cl);
+        Player player2 = new Player("Bさん", cl);
+        while(true){
+            if(plyer1.totalValue() <= 21){
+                plyer1.draw();
+            }
+            if(player2.totalValue() <= 21){
+                player2.draw();
+            }
+            if(plyer1.totalValue() == 21){
+                System.out.println(plyer1.name + "の勝ち");
+                break;
+            }
+            if(player2.totalValue() == 21){
+                System.out.println(player2.name + "の勝ち");
+                break;
+            }
+            if(plyer1.totalValue() > 21){
+                System.out.println(player2.name + "の勝ち");
+                break;
+            }
+            if(player2.totalValue() > 21){
+                System.out.println(plyer1.name + "の勝ち");
+                break;
+            }
+        }
+        
+         //プレイヤー名を具体的に書く
+        //Player plyer2 = new Player("Bさん", cl);
 
 
     }
