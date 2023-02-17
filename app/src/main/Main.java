34879e2e39baddfa449b2fe9f737ce8783deaf1e
ogiek0628 <117128611+ogiek0628@users.noreply.github.com>
2023-02-17 11:35:36 +0900

    実装例

--- app/src/main/Main.java
+++ app/src/main/Main.java
@@ -1,43 +1,65 @@
 package jp.ac.uryukyu.ie.e225752_e225755_e225758;
+import jp.ac.uryukyu.ie.e225752_e225755_e225758.Cardlist;
+import jp.ac.uryukyu.ie.e225752_e225755_e225758.Player;
 
-public class Main {
+import java.nio.file.SecureDirectoryStream;
+import java.util.Scanner;
+
+public class Main  {
     public static void main(String[] args){
-        //プレイヤーが二人の場合
-        Cardlist cl = new Cardlist();
-        cl.Card_remove("Color Joker");
-        cl.Card_remove("Black Joker");
-        Player plyer1 = new Player("Aさん", cl);
-        Player player2 = new Player("Bさん", cl);
+        Cardlist clist = new Cardlist();
+        Player P1 = new Player("P1", clist);
+        Player P2 = new Player("P2", clist);
+        Dealer Dl = new Dealer("D1", clist);
+        Scanner scanner = new Scanner(System.in);
         while(true){
-            if(plyer1.totalValue() <= 21){
-                plyer1.draw();
+            
+            if(P1.totalValue() <= 21 || P2.totalValue() <= 21){
+                System.out.println("1.引く。⒉．引かない");
+                System.out.print(P1.name+"はカードを引きますか？ =>");
+                int selec_number_PL1 = scanner.nextInt();
+                System.out.println();
+                System.out.print(P2.name+"はカードを引きますか？ =>");
+                int selec_number_PL2 = scanner.nextInt();
+                if(selec_number_PL1 == 1){
+                    P1.draw();
+                }
+                if(selec_number_PL2 == 1){
+                    P2.draw();
+                }
+                if(selec_number_PL1 == 2 && selec_number_PL2 == 2){
+                    System.out.println("ディーラーのターン");
+                    Dl.dealerAutoDraw();
+                
+                    
+                    int hikaku_P1 = P1.totalValue() - Dl.totalValue();
+                    int hikaku_P2 = P2.totalValue() - Dl.totalValue();
+                    if(hikaku_P1 > 0 && hikaku_P2 > 0){
+                        System.out.println(P1.name+"と"+P2.name+"の勝ち");
+                       break;
+                    }
+                    if(hikaku_P1 > 0 && hikaku_P2 < 0){
+                        System.out.println(P1.name+"の勝ち");
+                        break;
+                    }
+                    if(hikaku_P2 > 0 && hikaku_P1 < 0){
+                        System.out.println(P2.name+"の勝ち");
+                        break;
+                    }   
+                    if(hikaku_P1 == 0 && hikaku_P2 == 0){
+                        System.out.println("同点");
+                        break;
+                    }
+                    if(hikaku_P1 < 0 && hikaku_P2 < 0){                            
+                        System.out.println("ディーラーの勝ち");
+                        break;
+                    }
+                      
+                }
+                
             }
-            if(player2.totalValue() <= 21){
-                player2.draw();
-            }
-            if(plyer1.totalValue() == 21){
-                System.out.println(plyer1.name + "の勝ち");
-                break;
-            }
-            if(player2.totalValue() == 21){
-                System.out.println(player2.name + "の勝ち");
-                break;
-            }
-            if(plyer1.totalValue() > 21){
-                System.out.println(player2.name + "の勝ち");
-                break;
-            }
-            if(player2.totalValue() > 21){
-                System.out.println(plyer1.name + "の勝ち");
-                break;
-            }
+            
+            
         }
-        
-         //プレイヤー名を具体的に書く
-        //Player plyer2 = new Player("Bさん", cl);
-
-
     }
-    
-    
 }
